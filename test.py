9986a0f864fe91c69cf3fa2125f340c4c838d8cb
Neal Hayes <neal.hayes@skybettingandgaming.com>
2018-09-04 12:29:23 +0100

    [refs NPH-1] moved saus to its own file.

--- test.py
+++ test.py
@@ -203,29 +203,7 @@ for X in range(1, 14):
 '''
 
 # Do something while the time is in a certain window
-
-if (time.strftime("%H:%M:%S")) > (time.strftime("14:00:00")):
-    print("Outside of acceptable trigger window. " +
-          "Must be triggeed before 14:00"
-          )
-    exit(1)
-else:
-    print("Within acceptable time bounds. Continuing.")
-
-while (time.strftime("%H:%M:%S")) < (time.strftime("10:50:00")):
-    print("The current time is:", time.strftime("%H:%M:%S") +
-          " - too early to trigger")
-    time.sleep(10)
-
-print("Ready to trigger now the time is:", time.strftime("%H:%M:%S"))
-
-print("    _(_)(_)(_)(_)_      _(_)_     (_)          (_) _(_)(_)(_)(_)_   ")
-print("   (_)          (_)   _(_) (_)_   (_)          (_)(_)          (_)  ")
-print("   (_)_  _  _  _    _(_)     (_)_ (_)          (_)(_)_  _  _  _     ")
-print("     (_)(_)(_)(_)_ (_) _  _  _ (_)(_)          (_)  (_)(_)(_)(_)_   ")
-print("    _           (_)(_)(_)(_)(_)(_)(_)          (_) _           (_)  ")
-print("   (_)_  _  _  _(_)(_)         (_)(_)_  _  _  _(_)(_)_  _  _  _(_)  ")
-print("     (_)(_)(_)(_)  (_)         (_)  (_)(_)(_)(_)    (_)(_)(_)(_)    ")
+# See saus.py
 
 
 '''
